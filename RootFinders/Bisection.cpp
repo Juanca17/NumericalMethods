a08f9f27ef85515248f394c8b111542c0a43ea57
Juanca <juancasan17@gmail.com>
2017-05-22 18:26:45 -0500

    Bairstow, Bisection, NewtonRaphson, Secant

--- RootFinders/Bisection.cpp
+++ RootFinders/Bisection.cpp
@@ -1,88 +1,47 @@
-#include <iostream>
-#include <iomanip> // setprecision
+#include <iomanip>
 #include <cmath>
-
-#define PRECISION 6
-
 using namespace std;
 
-void tabula(double a, double b);
 double f(double x);
+void bisection(double a, double b, double tolerance);
 
-int main()
-{
-   cout << setprecision(PRECISION);
-   cout << "\nCalculo de las raices de una funcion aplicando el metodo de la biseccion\n";
-   cout << "\nIngrese el intervalo inicial [a,b]:" << endl;
+int main() {
+  double  a = 4,
+          b = 5,
+          tolerance = 0.001;
 
-   double a, b, tolerancia;
+  printf ("\nRoot finding by Bisection Method\n");
 
-   cout << "\na = ";
-   cin >> a;
+  bisection(a, b, tolerance);
 
-   cout << "b = ";
-   cin >> b;
-
-   tabula(a, b);
-
-   cout << "\nEscoja el intervalo adecuado" << endl;
-   cout << "\na = ";
-   cin >> a;
-
-   cout << "b = ";
-   cin >> b;
-
-   double xr;
-
-   if (f(a) * f(b) > 0) {
-      cout << "\nNo se puede aplicar el metodo de la biseccion\n";
-      cout << "porque f(" << a << ") y f(" << b << ") tienen el mismo signo" << endl;
-
-   } else {
-      cout << "Tolerancia = ";
-      cin >> tolerancia;
-
-      cout << "\na\tb\tx\tf(a)\t\tf(b)\t\tf(x)\n" << endl;
-      do {
-
-         xr = (a + b) / 2.0;
-         cout << a << "\t" << b << "\t" << xr << "\t" << f(a) << "\t" << f(b) << "\t" << f(xr) << endl;
-
-         if (fabs(f(xr)) <= tolerancia) {
-            cout << "\n\nPara una tolerancia de " << tolerancia << " la raiz de f es: " << xr << endl;
-            break;
-
-         } else {
-            if (f(xr) * f(a) > 0) {
-               a = xr;
-            } else if (f(xr) * f(b) > 0) {
-               b = xr;
-            }
-         }
-
-      } while (1);
-   }
-
-   cin.get();
-   cin.get();
-   return 0;
+  return 0;
 }
 
-#define INTERVALOS 10
-void tabula(double a, double b)
-{
-   int puntos = INTERVALOS + 1;
-
-   double ancho = (b - a) / INTERVALOS;
-
-   cout << "\n\tx\tf(x) " << endl;
-   for (int i = 0; i < puntos; i++) {
-      cout << "\t" << a << "\t" << f(a) << endl;
-      a = a + ancho;
-   }
+double f(double x) {
+  return sin(x) - (0.25*x) + 2;
 }
 
-double f(double x)
-{
-   return exp(-1 * x) - cos(3 * x) - 0.5;
+void bisection(double a, double b, double tolerance) {
+  double xr, error;
+  error = tolerance + 1;
+
+  if (f(a) * f(b) > 0) {
+    printf ("f(%g) and f(%g) do not enclose a root\n\n",a,b);
+    return;
+  }
+
+  printf ("\na\tb\tx\tf(a) \t\tf(b) \t\tf(x)\t\terror\n");
+  while (error > tolerance) {
+    xr = (a + b) / 2.0;
+    error = fabs(f(xr));
+
+    printf ("%g\t%g\t%g\t%.6g \t%.6g \t %.6g\t%0.4f\n",a,b,xr,f(a),f(b),f(xr),error);
+    if (f(xr) * f(a) > 0) {
+      a = xr;
+    } else if (f(xr) * f(b) > 0) {
+      b = xr;
+    }
+  }
+  printf ("\n\nFor a tolerance of %g the root of is: %g\n",tolerance,xr);
+
 }
