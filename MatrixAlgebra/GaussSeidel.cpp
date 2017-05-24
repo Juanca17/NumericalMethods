afa1d197cbaedb7de0d049f4e378653c5315404c
Juanca <juancasan17@gmail.com>
2017-05-24 01:27:15 -0500

    Gauss-Seidel

---
+++ MatrixAlgebra/GaussSeidel.cpp
@@ -0,0 +1,64 @@
+#include <iomanip>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+const int n = 3;
+
+void gauss_seidel(double[][n], double[], double tolerance, int maxIterations);
+
+int main() {
+  double  a[n][n] = {{3,-0.1,-0.2},
+                    {0.1,7,-0.3},
+                    {0.3,-0.2,10}},
+          b[n] = {7.85,-19.3,71.4},
+          tolerance = 0.00001;
+  int maxIterations = 100;
+
+
+  printf ("\nMatrix Algebra by Gauss–Seidel method\n");
+
+  gauss_seidel(a, b, tolerance, maxIterations);
+
+  return 0;
+}
+
+void gauss_seidel(double a[][n], double b[], double tolerance, int maxIterations) {
+  double y[10];
+  double e1, e2, e3;
+  int iteration = 0, i = 0, j = 0;
+  int auxiter = maxIterations;
+  double x[3] = {0, 0, 0};
+  e1 = e2 = e3 = tolerance + 1;
+
+  while (maxIterations > 0) {
+    for (i = 0; i < n; i++) {
+      y[i] = (b[i] / a[i][i]);
+
+      for (j = 0; j < n; j++) {
+        if (j == i) {
+          continue;
+        }
+
+        y[i] = y[i] - ((a[i][j] / a[i][i]) * x[j]);
+        x[i] = y[i];
+      }
+    }
+
+    e1 = b[0] - ((a[0][0]*y[0]) + (a[0][1]*y[1]) + (a[0][2]*y[2]));
+    e2 = b[1] - ((a[1][0]*y[0]) + (a[1][1]*y[1]) + (a[1][2]*y[2]));
+    e3 = b[2] - ((a[2][0]*y[0]) + (a[2][1]*y[1]) + (a[2][2]*y[2]));
+
+    if((fabs(e1) < tolerance) && (fabs(e2) < tolerance) && (fabs(e3) < tolerance)){
+      for(i = 0; i < n ; i++){
+        printf("x%d = %g\n",i+1,y[i]);
+      }
+      printf("\nIterations: %d\n\n",auxiter - maxIterations);
+      return;
+    }
+
+    maxIterations--;
+  }
+  printf ("\n\nThe maximum amount of allowed iterations was exceeded\n\n");
+
+}
